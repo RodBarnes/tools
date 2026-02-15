5a3d86c765a8f41df980cb6735211f5e9c660f1f
AdvApp <advappsw@gmail.com>
2026-02-15 15:37:42 -0700

    Fixed -n, dquate variables, let stderr out.

--- device.sh
+++ device.sh
@@ -4,10 +4,10 @@
 
 mount_device_at_path() {
   local device=$1 mount=$2 dir=$3
-  
+
   # Ensure mount point exists
-  if [ ! -d $mount ]; then
-    sudo mkdir -p $mount &> /dev/null
+  if [ ! -d "$mount" ]; then
+    sudo mkdir -p "$mount" > /dev/null
     if [ $? -ne 0 ]; then
       showx "Unable to locate or create '$mount'." >&2
       exit 2
@@ -15,15 +15,15 @@ mount_device_at_path() {
   fi
 
   # Attempt to mount the device
-  sudo mount $device $mount &> /dev/null
+  sudo mount "$device" "$mount" > /dev/null
   if [ $? -ne 0 ]; then
     showx "Unable to mount the device '$device'." >&2
     exit 2
   fi
 
-  if [ ! -z $dir ] && [ ! -d "$mount/$dir" ]; then
+  if [ -n "$dir" ] && [ ! -d "$mount/$dir" ]; then
     # Ensure the directory structure exists
-    sudo mkdir "$mount/$dir" &> /dev/null
+    sudo mkdir "$mount/$dir" > /dev/null
     if [ $? -ne 0 ]; then
       showx "Unable to locate or create '$mount/$dir'." >&2
       exit 2
@@ -35,8 +35,8 @@ unmount_device_at_path() {
   local path=$1
 
   # Unmount if mounted
-  if mountpoint -q $path; then
-    sudo umount $path &> /dev/null
+  if mountpoint -q "$path"; then
+    sudo umount "$path" > /dev/null
     if [ $? -ne 0 ]; then
       showx "Unable to locate or unmount '$path'." >&2
       exit 2
